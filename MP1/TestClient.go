487429387ea5fe195732af4b7082905a2de64fbd
dipayan2 <dipayan2@illinois.edu>
2019-09-15 00:28:35 -0500

    Testing -- not complete: Don't Test

---
+++ MP1/TestClient.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"net"
+	"os"
+	"bufio"
+	"regexp"
+	"fmt"
+	"io"
+	"log"
+	"strings"
+	"strconv"
+)
+
+func client() {
+	ln, _ := net.Listen("tcp", ":8080")
+	for {
+		conn, _ := ln.Accept()
+		defer conn.Close()
+
+		conn_reader := bufio.NewReader(conn)
+
+		params, _ := conn_reader.ReadString('\n')
+		params = params[:len(params)-1]
+		params_list := strings.Split(params, ",")
+		filename, pattern := params_list[0], params_list[1]
+
+		r, _ := regexp.Compile(pattern)
+		file, err := os.Open(filename)
+		if err != nil {
+			log.Fatal("Error: Couldn't access the file\n")
+		}
+		defer file.Close()
+
+		file_reader := bufio.NewReader(file)
+
+		var linenum int = 0
+		for {
+			line, err := file_reader.ReadString('\n')
+			linenum += 1
+			if r.MatchString(line) {
+				matched_line := fmt.Sprintf("%d$$$$%s", linenum, line)
+				fmt.Fprintf(conn, matched_line)
+			}
+			if err != nil {
+				if err != io.EOF {
+					fmt.Printf("Unknown error while reading file %s", filename)
+				}
+				break
+			}
+		}
+		fmt.Fprintf(conn, "<EOF>" + "\n")
+		conn.Close()
+	}
+}
+
+func file_recv() {
+	connection, err := net.Dial("tcp", "localhost:27001")
+	if err != nil {
+		panic(err)
+	}
+	defer connection.Close()
+	fmt.Println("Connected to server, start receiving the file name and file size")
+	bufferFileName := make([]byte, 64)
+	bufferFileSize := make([]byte, 10)
+	
+	connection.Read(bufferFileSize)
+	fileSize, _ := strconv.ParseInt(strings.Trim(string(bufferFileSize), ":"), 10, 64)
+	
+	connection.Read(bufferFileName)
+	fileName := strings.Trim(string(bufferFileName), ":")
+	
+	newFile, err := os.Create(fileName)
+	
+	if err != nil {
+		panic(err)
+	}
+	defer newFile.Close()
+	var receivedBytes int64
+	
+	for {
+		if (fileSize - receivedBytes) < BUFFERSIZE {
+			io.CopyN(newFile, connection, (fileSize - receivedBytes))
+			connection.Read(make([]byte, (receivedBytes+BUFFERSIZE)-fileSize))
+			break
+		}
+		io.CopyN(newFile, connection, BUFFERSIZE)
+		receivedBytes += BUFFERSIZE
+	}
+	fmt.Println("Received file completely!")
+}
+const BUFFERSIZE = 1024
+
+func main() {
+	//client()
+	file_recv()
+}
--- MP1/TestFile.go
+++ MP1/TestFile.go
@@ -190,11 +190,20 @@ func Test1(serverMap map[string]int, pattern string, filePrefix string, terminal
 
    		err = f.Close()
 
-   		outAddr := serverIP+":/home/cs425/MP1"
-   		fmt.Printf("Trying %s\n",string(outAddr))
-		cmdStr := "scp "+string(filename)+" "+string(outAddr)
-   		_, err = exec.Command(string(cmdStr)).Output()
+  //  		outAddr := "dipayan2@"+serverIP+":/home/cs425/MP1"
+  //  		fmt.Printf("Trying %s\n",string(outAddr))
+		// cmdStr := "scp "+string(filename)+" "+string(outAddr)
+  //  		_, err = exec.Command(string(cmdStr)).Output()
 
+   		// for o := 1; o <= 10; o++ {
+   		// 	command := fmt.Sprintf("")
+   		// 	_, err = exec.Command("scp testvm." + strconv)
+
+   		// }
+   		fmt.Printf(serverIP)
+
+   		command := fmt.Sprintf("scp testvm.%d.log $vm01:/home/cs425/MP1", fileIdx)
+   		_, err = exec.Command(command).Output()
 
 
    		if err != nil {
@@ -210,8 +219,76 @@ func Test1(serverMap map[string]int, pattern string, filePrefix string, terminal
 	}
 
 	distributedGrep(serverMap,pattern,filePrefix,terminal)
+	 // Check the Output with set Patterns
+
+
 
 	
+}
+
+const BUFFERSIZE = 1024
+
+func fillString(retunString string, toLength int) string {
+	for {
+		lengtString := len(retunString)
+		if lengtString < toLength {
+			retunString = retunString + ":"
+			continue
+		}
+		break
+	}
+	return retunString
+}
+
+func sendFileToClient(connection net.Conn) {
+	fmt.Println("A client has connected!")
+	defer connection.Close()
+	file, err := os.Open("dummyfile.dat")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fileInfo, err := file.Stat()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fileSize := fillString(strconv.FormatInt(fileInfo.Size(), 10), 10)
+	fileName := fillString(fileInfo.Name(), 64)
+	fmt.Println("Sending filename and filesize!")
+	connection.Write([]byte(fileSize))
+	connection.Write([]byte(fileName))
+	sendBuffer := make([]byte, BUFFERSIZE)
+	fmt.Println("Start sending file!")
+	for {
+		_, err = file.Read(sendBuffer)
+		if err == io.EOF {
+			break
+		}
+		connection.Write(sendBuffer)
+	}
+	fmt.Println("File has been sent, closing connection!")
+	return
+}
+
+
+func file_server() {
+	server, err := net.Listen("tcp", "localhost:27001")
+	if err != nil {
+		fmt.Println("Error listetning: ", err)
+		os.Exit(1)
+	}
+	defer server.Close()
+	fmt.Println("Server started! Waiting for connections...")
+	for {
+		connection, err := server.Accept()
+		if err != nil {
+			fmt.Println("Error: ", err)
+			os.Exit(1)
+		}
+		fmt.Println("Client connected")
+		go sendFileToClient(connection)
+	}
 }
 
 func main(){
@@ -227,7 +304,10 @@ func main(){
 
 	// Testing part
 	// -- Create the log files and send to vms
-	Test1(serverMap,*pattern,*filePrefix,*terminal)
+	//Test1(serverMap,*pattern,*filePrefix,*terminal)
+
+	file_server()
+
 	
 
 }
