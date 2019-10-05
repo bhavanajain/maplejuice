9e5de0f3512972bf8ee01f34314a81a01a8f9dbf
dipayan2 <dipayan2@illinois.edu>
2019-10-05 07:09:11 -0500

    Introducer Handled

--- MP2/membership.go
+++ MP2/membership.go
@@ -739,9 +739,8 @@ func listenOtherPort() (err error) {
 					memberMap[subject] = &newnode
 					glog.Infof("[INTRODUCER %d] Received an introducer message from %d",0,subject)
 					message := fmt.Sprintf("JOIN,%d,%s,%d", 0, memberMap[0].ip,memberMap[0].timestamp)
-					time.Sleep(2*time.Second)
+					time.Sleep(6*time.Second)  // beacuse in listen we wait for 5 seconds
 					updateMonitors()
-					glog.Infof("[MONITOR] Updated Monitors")
 					disseminate(message)
 
 				}
