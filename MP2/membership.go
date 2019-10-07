44161e72ede1ff4405d38ae3de25733a886e8060
Bhavana Jain <cs14btech11008@iith.ac.in>
2019-10-06 19:25:15 -0500

    Test p=0.03

--- MP2/membership.go
+++ MP2/membership.go
@@ -864,17 +864,10 @@ func getmyIP() (string) {
 	return myip
 }
 
-func usage() {
-	fmt.Fprintf(os.Stderr, "usage: example -stderrthreshold=[INFO|WARN|FATAL] -log_dir=[string]\n", )
-	flag.PrintDefaults()
-	os.Exit(2)
-}
 
 func init() {
-	flag.Usage = usage
 	flag.Parse()
 }
-
 
 func main() {
 
@@ -928,6 +921,9 @@ func main() {
 			wg.Done()
 		}
 	}()
+
+
+	glog.Flush()
 
 	wg.Wait()
 	return
