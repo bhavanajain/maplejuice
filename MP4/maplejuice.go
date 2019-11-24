1277015a1279084d1951be007f310f0be87ca4c8
dipayan2 <dipayan2@illinois.edu>
2019-11-24 11:55:15 -0600

    Execute Command

--- MP4/maplejuice.go
+++ MP4/maplejuice.go
@@ -14,6 +14,7 @@ import (
     "io/ioutil"
     "math/rand"
     "errors"
+    "os/exec"
     // "math"
 )
 
@@ -1615,6 +1616,39 @@ func LeaderHandler( subject int, newPort int) {
 
     }(subject)
     return
+}
+
+
+
+func ExecuteCommand(comd string, outputfile string) {
+    // split_command := strings.Split(comd, " ")
+
+
+
+    // cmd := exec.Command("echo", "'WHAT THE HECK IS UP'")
+    cmd := exec.Command("sh","-c",comd)
+    // open the out file for writing
+    outfile, err := os.Create(outputfile)
+    if err != nil {
+        panic(err)
+    }
+    defer outfile.Close()
+
+    stdoutPipe, err := cmd.StdoutPipe()
+    if err != nil {
+        panic(err)
+    }
+
+    writer := bufio.NewWriter(outfile)
+    defer writer.Flush()
+
+    err = cmd.Start()
+    if err != nil {
+        panic(err)
+    }
+
+    go io.Copy(writer, stdoutPipe)
+    cmd.Wait()
 }
 
 
