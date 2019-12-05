64f15fff64180fa9484976d7828f339bc9131a7b
dipayan2 <dipayan2@illinois.edu>
2019-12-05 15:39:14 -0600

    Updates

--- MP4/maplejuice.go
+++ MP4/maplejuice.go
@@ -804,11 +804,13 @@ func listenMasterRequests() {
                 }
 
             case "keyack":
-                fmt.Printf("keyack RECVD\n")
+                
                 keyCount = keyCount -1
                 key := split_message[1]
                 keyStatus[key] = DONE
                 log.Printf("keyack RECVD %s , remaining keys %d \n",key,keyCount)
+                fmt.Printf("keyack RECVD %s , remaining keys %d \n",key,keyCount)
+
                 fmt.Printf("%s key has been processed and the corresponding sdfs is added\n", key)
 
                 success := true
